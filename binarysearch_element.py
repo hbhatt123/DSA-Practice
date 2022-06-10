29968ff11f80cd62df1378c54992c22dd1de6864
Himani Bhatt <32537184+hbhatt123@users.noreply.github.com>
2022-06-10 12:46:07 +0530

    Create binarysearch_element.py

---
+++ binarysearch_element.py
@@ -0,0 +1,24 @@
+# Online Python-3 Compiler (Interpreter)
+print ("Hello, World!")
+
+def binarysearch(ls, x):
+    n = len(ls)
+    low = 0
+    high = n-1
+    while (low <= high):
+        mid = int((low + high)/2)
+        if ls[mid] == x:
+            return mid
+        elif ls[mid] > x:
+            high = mid -1
+        else:
+            low = mid +1
+            
+        
+if __name__ == '__main__':
+    ls = [10,20,30,40,50,60]
+    x = 10
+    res = binarysearch(ls, x)
+    print(res)
+    
+    
