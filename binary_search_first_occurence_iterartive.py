830aa312b33679ef33883a2f91c8b030d0f47a8b
Himani Bhatt <32537184+hbhatt123@users.noreply.github.com>
2022-06-11 09:04:34 +0530

    Create binary_search_first_occurence_iterartive.py

---
+++ binary_search_first_occurence_iterartive.py
@@ -0,0 +1,31 @@
+# Online Python-3 Compiler (Interpreter)
+print ("Hello, World!")
+
+def binarysearch(ls, x, low, high):
+    
+    while (low <= high):
+        mid = int((low + high)/2)
+        if ls[mid] > x:
+            high = mid -1
+        elif ls[mid] < x:
+            low = mid +1
+        else:
+            if ls[mid] == x and ls[mid-1] != x:
+                return mid
+            elif ls[mid] == x and ls[mid-1] == x:
+                high = mid -1
+    return -1        
+            
+        
+if __name__ == '__main__':
+    infi = float('inf')
+    ls = [10,20,20,30,30,40,40,50,60]
+    # ls = [1,3,2,5,2,infi,infi,infi,infi,infi,infi]
+    n = len(ls)
+    low = 0
+    high = n-1
+    x = 20
+    res = binarysearch(ls, x,low,high)
+    print(res)
+    
+    
