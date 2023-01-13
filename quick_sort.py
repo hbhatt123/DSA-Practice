911dbe1aab0de9e42dfd006b0c25933b5cab2149
Himani Bhatt <32537184+hbhatt123@users.noreply.github.com>
2023-01-13 21:20:15 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -4,28 +4,34 @@ def partition(ls,l,h):
     p = ls[l]
     i = l
     j = h
+    print("i",i,"j",j)
     print("pivot",p)
     while (i <= j):
-        while p >= ls[i]:
+        while i<=j and p >= ls[i]:
             i = i + 1
+            print("i",i)
         while p < ls[j]:
             j = j - 1
+        print("j",j)
+        print(i,j)
         if i < j:
             ls[j], ls[i] = ls[i], ls[j]
     ls[l],ls[j] = ls[j],ls[l]
+    print(ls)
     return j 
 def quick_sort(ls,l,h):
     if (l < h):
         j = partition(ls,l,h)
-        quick_sort(ls,l,j)
+        quick_sort(ls,l,j-1)
         quick_sort(ls,j+1,h)
     return ls    
    
    
 
 if __name__=='__main__':
-    Lst = [10,16,8,12,1,6,3,9,5,11,4,2,21]
+    Lst = [6,10,12,1]
     n = len(Lst)
+    print(Lst)
    
     res = quick_sort(Lst,0,n-1)
     print(res)
