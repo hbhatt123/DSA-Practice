7348105dabc73dc1ecc7e298d8e62788b116bc2c
Himani Bhatt <32537184+hbhatt123@users.noreply.github.com>
2024-01-01 13:31:16 +0530

    Create two_sum.py
    using hashmap

---
+++ two_sum.py
@@ -0,0 +1,15 @@
+def read(n: int, book: [int], target: int) -> str:
+
+  
+    map = {}
+
+    for i in range(n):
+        a = book[i] 
+        more = target - a
+
+        if more in map:
+           return 'YES'
+
+        map[a] = i
+
+    return 'NO'    
